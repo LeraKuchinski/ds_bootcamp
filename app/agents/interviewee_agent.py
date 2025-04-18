d0b95375e6ea58abe4809322e2f144525a4bec22
LeraKuchinski <89159163+LeraKuchinski@users.noreply.github.com>
2025-04-18 23:52:54 +0300

    Update interviewee_agent.py

--- app/agents/interviewee_agent.py
+++ app/agents/interviewee_agent.py
@@ -3,8 +3,10 @@ from app.agents.tools.lie_answer import lie_answer
 
 def create_interviewee_agent(system_prompt: str) -> Agent:
     return Agent(
-        name="Агент кандидата на работу",
-        handoff_description="Ты кандидат, который отвечает на вопросы на основе персоны и проверяемого навыка",
+        name="Стеснительный кандидат",
+        handoff_description="Ты кандидат, который отвечает на вопросы очень стеснительно"
+        # handoff_description="Ты кандидат, который отвечает на вопросы на основе персоны и проверяемого навыка",
         instructions=system_prompt,
         tools=[lie_answer],
     )
+    
