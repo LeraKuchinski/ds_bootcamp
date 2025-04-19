5ccd160a11cc77ae64e7ea12bb330ccbc19b8bf8
Denis123-commit1 <financialstudent1@gmail.com>
2025-04-19 05:09:05 +0300

    FIX: Нейронка не отвечает 'Чем могу помочь и тп'. Про стеснительного кандидата убрал. Это логика в психотипах зашита будет

--- app/agents/interviewee_agent.py
+++ app/agents/interviewee_agent.py
@@ -5,14 +5,18 @@ from app.agents.profiles import candidate_profiles
 
 
 
-def create_interviewee_agent(system_prompt: str, psyho_profile: str, persona, skill) -> Agent:
+def create_interviewee_agent(system_prompt: str, psyho_profile: str, persona: str, skill: str) -> Agent:
     profile = candidate_profiles[psyho_profile]
+    profile_description = f"{profile['name']} — {profile['type']}. {profile['description']}"
+    
     return Agent(
-        name="Стеснительный кандидат",
-        handoff_description="Ты кандидат, который отвечает на вопросы очень стеснительно",
+        name="Кандидат на работу",
+        handoff_description="""Ты кандидат, который точно отвечает в соответствии с инструкцией
+        Кандидат, который следует психотипу и контексту запроса""", # Инструкция агенту - передача задач агентам если их несколько
         instructions=system_prompt.format(
-            persona=persona,  # <-- как в YAML-шаблоне
             name=profile['name'],
+            persona=persona,
+            profile_description=profile_description,
             skill=skill,
         ),
         tools=[lie_answer],
